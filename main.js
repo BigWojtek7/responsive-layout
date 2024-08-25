a7d52bbe6eb771d1dda759a76fe3255f412d5a76
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-08-25 13:08:54 +0200

    Restyle style.css $ main.js
    Delete blank spaces & unused commented code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const btnToggle = document.querySelector('.btn-toggle')
-const nav = document.querySelector('.nav')
+const btnToggle = document.querySelector('.btn-toggle');
+const nav = document.querySelector('.nav');
 
 btnToggle.addEventListener('click', () => {
-  nav.classList.toggle('nav-visible')
-})
+  nav.classList.toggle('nav-visible');
+});
